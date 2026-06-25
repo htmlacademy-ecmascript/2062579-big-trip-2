40eb1d66574341a614d8180b09a61d2b262b6b8c
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2026-06-25 18:16:23 +0300

    Добавляет в код поясняющие комментарии

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ const tripInfoContainer = document.querySelector('.trip-main');
 const filterContainer = document.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
 
-const eventsPresenter = new EventsPresenter({pointsListContainer: tripEventsContainer});
+const eventsPresenter = new EventsPresenter({pointsListContainer: tripEventsContainer}); // создаем презентер с указанием контейнера, в который он добавится
 
-render(new TripInfoView(), tripInfoContainer, RenderPosition.AFTERBEGIN);
-render(new FilterView(), filterContainer);
-render(new SortingView(), tripEventsContainer);
+render(new TripInfoView(), tripInfoContainer, RenderPosition.AFTERBEGIN); // добавляем информацию о маршруте
+render(new FilterView(), filterContainer); // добавляем фильтры
+render(new SortingView(), tripEventsContainer); // добавляем сортировку
 
-eventsPresenter.init();
+eventsPresenter.init(); // добавляем презентер с маршрутом
--- src/view/events-presenter.js
+++ src/view/events-presenter.js
@@ -5,21 +5,21 @@ import PointView from './point-view.js';
 import { RenderPosition, render } from '../render.js';
 
 export default class EventsPresenter {
-  eventsPoint = new PointView();
-  pointsList = new PointsListView();
+  eventsPoint = new PointView(); // точка маршрута
+  pointsList = new PointsListView(); // список для точек маршрута
 
   constructor({pointsListContainer}) {
-    this.pointsListContainer = pointsListContainer;
+    this.pointsListContainer = pointsListContainer; // получаем контейнер, в который будет вставлен список точек
   }
 
   init() {
-    render(this.pointsList, this.pointsListContainer);
+    render(this.pointsList, this.pointsListContainer); // вставляем список в контейнер
 
     for(let i = 0; i < 3; i++) {
-      render(new PointView(), this.pointsList.getElement());
+      render(new PointView(), this.pointsList.getElement()); // добавляем в список три точки
     }
 
-    render(new EditPointView(), this.pointsList.getElement(), RenderPosition.AFTERBEGIN);
-    render(new AddNewPointView(), this.pointsList.getElement());
+    render(new EditPointView(), this.pointsList.getElement(), RenderPosition.AFTERBEGIN); // вставляем в начало списка форму редактирования точки
+    render(new AddNewPointView(), this.pointsList.getElement()); // вставляем в список форму добавления новой точки
   }
 }
